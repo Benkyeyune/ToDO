1a2787b7142bea70af53745c6401df4219180fb1
Bamwesigye-Calvin-Kiiza <bamwesigyecalvinkiiza@gmail.com>
2023-04-22 17:59:39 +0300

    done

--- To-Do-App/to_do_app/ToDoApp/urls.py
+++ To-Do-App/to_do_app/ToDoApp/urls.py
@@ -6,12 +6,12 @@ from django.contrib.auth.views import LogoutView
 app_name = 'ToDoApp'
 
 urlpatterns = [
-    # path('',views.landingpage,name='landingpage'),
+    path('',views.landingpage,name='landingpage'),
     path('home/',Home.as_view(),name = 'home'),
     path('listview/',TaskListView.as_view(),name = 'listview'),
     # path('register/',register.as_view(),name='register'),
     path('register/',views.register,name='register'),
-    path('',user_login.as_view(),name ='login'),
+    path('login/',user_login.as_view(),name ='login'),
     path('logout/',LogoutView.as_view(next_page='ToDoApp:login'),name ='logout'),
     path('addTask/',addTask.as_view(),name='addTask'),
     path('task-update<int:pk>/', TaskUpdate.as_view(), name='task-update'),
